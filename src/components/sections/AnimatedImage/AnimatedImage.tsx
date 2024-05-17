2ba3109af67f2bd19b4f3d6b367265d5bf733fdc
quni <quni.inne@gmail.com>
2024-05-17 04:13:43 +0200

    feat: WORKING HERO ANIMATION (slighly off but still) | and framer motion removal

--- src/components/sections/AnimatedImage/AnimatedImage.tsx
+++ src/components/sections/AnimatedImage/AnimatedImage.tsx
@@ -1,25 +1,48 @@
 import background from "../../../assets/background.webp";
-import { animated } from "@react-spring/web";
+import { animated, useScroll, useSpring } from "@react-spring/web";
 
 interface AnimatedImageProps {}
 
 const AnimatedImage: React.FC<AnimatedImageProps> = ({}) => {
+  const { scrollYProgress } = useScroll();
+
+  const imageClip = useSpring({
+    clipPath: scrollYProgress.to(
+      [0, 0.2],
+      ["inset(0% 0% 0% 0%)", "inset(25% 0% 25% 0%)"],
+    ),
+  });
+
+  const imageTransform = useSpring({
+    transform: scrollYProgress.to(
+      [0, 0.4], // Adjust range to start after clipPath
+      ["translateX(0%)", "translateX(50%)"],
+    ),
+  });
+
+  const sectionCrop = useSpring({
+    height: scrollYProgress.to([0, 0.2], ["100vh", "10vh"]),
+  });
+
   return (
-    <>
+    <animated.div
+      className="relative w-screen overflow-hidden"
+      style={sectionCrop}
+    >
       <animated.section
         id="hero"
-        className="flex flex-col min-h-screen h-[100dvh] justify-center w-screen overflow-x-hidden overflow-y-hidden"
+        className="flex flex-col min-h-screen h-screen justify-center w-screen overflow-hidden top-0"
       >
         <animated.img
           src={background}
-          className="absolute top-0 left-0 h-screen object-cover object-left"
+          style={{ ...imageClip, ...imageTransform }}
+          className="absolute top-0 left-0 h-full w-full object-cover"
         />
         <animated.h1 className="text-4xl z-10 text-background text-center">
           Stanica Podkowa
         </animated.h1>
       </animated.section>
-    </>
+    </animated.div>
   );
 };
-
 export default AnimatedImage;
