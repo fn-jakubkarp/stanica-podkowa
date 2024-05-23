f6e57a066a1afda4344cd531491e97b727df0c39
quni <quni.inne@gmail.com>
2024-05-23 11:48:33 +0200

    feat: hide website end

--- src/components/UI/StickyContact/StickyContact.tsx
+++ src/components/UI/StickyContact/StickyContact.tsx
@@ -2,11 +2,8 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import { animated, useSpring } from "@react-spring/web";
 import { useState, useEffect } from "react";
+import { handleCall } from "../../../utils/handleCall";
 const StickyContact: React.FC = () => {
-  const handleCall = () => {
-    window.location.href = "tel:+48510058955";
-  };
-
   const [hidden, setHidden] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
 
@@ -15,10 +12,18 @@ const StickyContact: React.FC = () => {
       const currentScrollY = window.scrollY;
       const threshold = 250;
 
-      if (currentScrollY > lastScrollY && currentScrollY > threshold) {
-        setHidden(false); // Show when scrolling down
-      } else if (currentScrollY < lastScrollY) {
-        setHidden(true); // Hide when scrolling up
+      // Check if user has reached the bottom of the page
+      const isBottomOfPage =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight;
+
+      if (
+        currentScrollY > lastScrollY &&
+        currentScrollY > threshold &&
+        !isBottomOfPage
+      ) {
+        setHidden(false); // Show when scrolling down and not at the bottom
+      } else if (currentScrollY < lastScrollY || isBottomOfPage) {
+        setHidden(true); // Hide when scrolling up or at the bottom
       }
 
       setLastScrollY(currentScrollY);
@@ -37,17 +42,23 @@ const StickyContact: React.FC = () => {
 
   return (
     <animated.div
-      className="fixed flex justify-around bottom-0 transform -translate-x-1/2  bg-white text-text w-full z-50 h-[50px] sm:hidden"
+      className="fixed flex justify-around bottom-0 transform -translate-x-1/2 bg-white text-text w-full z-50 h-[50px] sm:hidden"
       style={stickySpring}
     >
-      <button className="border-r flex items-center justify-center w-1/4">
-        <FaMapMarkerAlt />
+      <button
+        className="border-r flex items-center justify-center w-1/4 hover:bg-gray-200 "
+        aria-label="Map"
+        role="button"
+      >
+        <FaMapMarkerAlt aria-hidden="true" />
       </button>
       <button
         onClick={handleCall}
-        className="flex items-center gap-2 w-3/4 justify-center"
+        className="flex items-center gap-2 w-3/4 justify-center hover:bg-gray-200 "
+        aria-label="Call for reservation"
+        role="button"
       >
-        <FaPhoneAlt /> Rezerwacja
+        <FaPhoneAlt aria-hidden="true" /> Rezerwacja
       </button>
     </animated.div>
   );
