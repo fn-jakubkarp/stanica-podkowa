ccbe939722f25e5329a67af4a45066dfb4d9cd9b
quni <quni.inne@gmail.com>
2024-05-23 21:37:44 +0200

    wip: outoffer - smoothening animations

--- src/components/sections/OurOffer/OurOffer.tsx
+++ src/components/sections/OurOffer/OurOffer.tsx
@@ -1,36 +1,56 @@
 import { useInView } from "react-intersection-observer";
 
-import React from "react";
+// Custom animations
+import { revealImageFromBelow, slidingText } from "../../../utils/animations";
+
+// Assets
 import macro from "../../../assets/macro.jpg";
-import { revealImageFromBelow } from "../../../utils/animations";
+import stary_dom from "../../../assets/stary_dom.jpg";
+
 interface OurOfferProps {}
 
 const OurOffer: React.FC<OurOfferProps> = () => {
-  const { ref, inView } = useInView({ threshold: 0.75 });
+  const { ref: imageRef, inView: imageView } = useInView({ threshold: 0.75 });
+  const { ref: textRef, inView: textView } = useInView({ threshold: 0.5 });
 
   return (
     <>
-      <section className="relative mt-[50px] flex w-screen flex-col items-center justify-center bg-secondary">
+      <section className="relative mt-20 flex w-screen flex-col items-center justify-center bg-secondary py-4">
         <div
           className="absolute left-8 top-[-50px] h-[100px] w-[100px] overflow-hidden bg-accent"
-          ref={ref}
+          ref={imageRef}
         >
           <img
             src={macro}
             className="relative left-0 top-0 h-full w-full object-cover"
             alt="Doniczka"
-            style={revealImageFromBelow(inView)}
+            style={revealImageFromBelow(imageView)}
           />
         </div>
 
         {/* Nasze, twoje domki */}
-        <div className="flex flex-col">
+        <div className="flex w-screen flex-col" ref={textRef}>
           <div className="mt-2 flex w-full justify-center font-open text-3xl font-light text-text-DARK">
             <span className="italic line-through">NASZE</span>
           </div>
           <div className="ml-6 flex w-full justify-center font-open text-3xl font-light text-text-DARK">
             <span className="mr-3 pl-12">TWOJE</span>
             <span>DOMKI</span>
+          </div>
+
+          <div className="overflow-hidden">
+            <p
+              style={slidingText(textView)}
+              className="font-joseph px-4 text-sm text-text-DARK"
+            >
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium corporis nisi nobis, nihil totam est illum fugiat
+              natus, vel temporibus facilis? Unde commodi maxime aspernatur
+              tenetur esse impedit sit. Consectetur?
+            </p>
+            <div className="p-4">
+              <img src={stary_dom} className="rounded-md" />
+            </div>
           </div>
         </div>
       </section>
--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -2,3 +2,10 @@ export const revealImageFromBelow = (inView: boolean) => ({
   transform: inView ? "translateY(0)" : "translateY(100%)",
   transition: "transform 1s",
 });
+
+export const slidingText = (inView: boolean) => ({
+  opacity: inView ? "1" : "0.5",
+  transform: inView ? "translateY(0)" : "translateY(50%)",
+  transition:
+    "transform 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000), opacity 1s cubic-bezier(0.215, 0.610, 0.355, 1.000)",
+});
