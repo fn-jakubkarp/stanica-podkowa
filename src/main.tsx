3aa99ae742e46dc8e73202732bceb39fbb0f1ec7
quni <quni.inne@gmail.com>
2024-05-24 13:08:16 +0200

    style: styling imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { injectSpeedInsights } from "@vercel/speed-insights";
-import { inject } from "@vercel/analytics";
 import App from "./App.tsx";
 import "./styles/main.css";
+
+// Analytics
+import { injectSpeedInsights } from "@vercel/speed-insights";
+import { inject } from "@vercel/analytics";
 
 // Vercel insights
 injectSpeedInsights();
