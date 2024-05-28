2a4fe0c944c37f5991d43029eff54e459e705ed6
quni <quni.inne@gmail.com>
2024-05-28 15:29:14 +0200

    styles: remove unused styles from card.tsx

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -11,7 +11,7 @@ const Card = ({ card }: { card: CardType }) => {
           className="aspect-video h-full max-h-60 w-full object-cover"
         />
         <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/50 to-transparent">
-          <div className="disabled p-4 pb-8 text-background">
+          <div className="disabled p-4 pb-8">
             <h3 className="mb-2 font-open text-3xl">{card.title}</h3>
             <p className="font-joseph flex flex-wrap pr-8 text-sm">
               {card.text}
