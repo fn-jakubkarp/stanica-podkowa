f9744f8d4abc6c592efee403edae8afe81a69c34
quni <quni.inne@gmail.com>
2024-05-29 18:19:22 +0200

    add: CardAttraction

--- src/components/UI/Card/CardAtractions.tsx
+++ src/components/UI/Card/CardAtractions.tsx
@@ -1,22 +1,50 @@
 import { CardType } from "./CardType";
 
-const CardSwiper = ({ card }: { card: CardType }) => {
-  const isEven = card.id % 2 === 0;
-
+const CardAtractions = ({ card }: { card: CardType }) => {
   return (
-    <div className="flex justify-center flex-col items-start">
-      {isEven && <p className=" pb-2 pl-2">{card.title}</p>}
-      <div className="">
-        <img
-          src={card.url}
-          alt={card.alt}
-          loading="lazy"
-          className="aspect-video h-full max-h-60 w-full object-cover"
-        />
+    <>
+      <div className="relative mt-6 flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <div className="p-6">
+          <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            {card.title}
+          </h5>
+          <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+            {card.text}
+          </p>
+        </div>
+        <div className="p-6 pt-0">
+          <a
+            className="!font-medium !text-blue-gray-900 !transition-colors hover:!text-text-DARK"
+            href={card.url}
+            target="_blank"
+          >
+            <button
+              className="flex select-none items-center gap-2 rounded-lg py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-text-DARK transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              data-ripple-dark="true"
+            >
+              Dowiedz się więcej
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                aria-hidden="true"
+                className="h-4 w-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                ></path>
+              </svg>
+            </button>
+          </a>
+        </div>
       </div>
-      {!isEven && <p className="pt-2 pl-2">{card.title}</p>}
-    </div>
+    </>
   );
 };
 
-export default CardSwiper;
+export default CardAtractions;
