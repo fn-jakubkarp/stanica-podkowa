eead702226713c11fef9d1810dfac447e76b64e1
quni <quni.inne@gmail.com>
2024-05-29 12:38:39 +0200

    add gallery assets

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -13,6 +13,7 @@ import oh_room from "../assets/PriceListAssets/Old/room.jpg";
 import pricelist_header from "../assets/PriceListAssets/header.jpg";
 // End of price list section
 // * ------ * //
+
 // Hero section
 import hero_accommodation from "../assets/HeroAssets/accommodation.jpg";
 import hero_events from "../assets/HeroAssets/events.jpg";
@@ -26,8 +27,41 @@ import hero_sauna from "../assets/HeroAssets/sauna.jpg";
 import hero_us from "../assets/HeroAssets/us.jpg";
 // * ------ * //
 
+// SVGS //
 import bullet_point from "../assets/svgs/bullet_point.svg";
 import logo from "../assets/svgs/logo.svg";
+// * ----- * //
+
+// Gallery
+// > Interior
+import nh_bathroom_top from "../assets/GalleryAssets/Interior/nh_bathroom_top.jpg";
+import nh_hall from "../assets/GalleryAssets/Interior/nh_hall.jpg";
+import nh_kitchen_closeup from "../assets/GalleryAssets/Interior/nh_kitchen_closeup.jpg";
+import nh_living_room_wide from "../assets/GalleryAssets/Interior/nh_living_room_wide.jpg";
+import nh_room_four_beds from "../assets/GalleryAssets/Interior/nh_room_four_beds.jpg";
+import nh_room_three_beds from "../assets/GalleryAssets/Interior/nh_room_three_beds.jpg";
+// > Mansion
+import m_arbor from "../assets/GalleryAssets/Mansion/m_arbor.jpg";
+import m_new_home from "../assets/GalleryAssets/Mansion/m_new_home.jpg";
+import m_outdoor_look from "../assets/GalleryAssets/Mansion/m_outdoor_look.jpg";
+// > Summer
+import s_arbor_entrance from "../assets/GalleryAssets/Summer/s_arbor_entrance.jpg";
+import s_grass from "../assets/GalleryAssets/Summer/s_grass.jpg";
+import s_hay_bale from "../assets/GalleryAssets/Summer/s_hay_bale.jpg";
+import s_playground_stairs from "../assets/GalleryAssets/Summer/s_playground_stairs.jpg";
+import s_sauna from "../assets/GalleryAssets/Summer/s_sauna.jpg";
+import s_signpost from "../assets/GalleryAssets/Summer/s_signpost.jpg";
+// > Winter
+import w_arbor from "../assets/GalleryAssets/Winter/w_arbor.jpg";
+import w_arbor_bottom from "../assets/GalleryAssets/Winter/w_arbor_bottom.jpg";
+import w_arbor_interior from "../assets/GalleryAssets/Winter/w_arbor_interior.jpg";
+import w_arbor_interior2 from "../assets/GalleryAssets/Winter/w_arbor_interior2.jpg";
+import w_arbor_top from "../assets/GalleryAssets/Winter/w_arbor_top.jpg";
+import w_oh_full from "../assets/GalleryAssets/Winter/w_oh_full.jpg";
+import w_oh_full2 from "../assets/GalleryAssets/Winter/w_oh_full2.jpg";
+import w_oh_icicle from "../assets/GalleryAssets/Winter/w_oh_icicle.jpg";
+import w_oh_terrace from "../assets/GalleryAssets/Winter/w_oh_terrace.jpg";
+import w_sauna from "../assets/GalleryAssets/Winter/w_sauna.jpg";
 
 export {
   bullet_point,
@@ -40,16 +74,41 @@ export {
   hero_night_sky,
   hero_old_house,
   hero_sauna,
+  hero_us,
   logo,
+  m_arbor,
+  m_new_home,
+  m_outdoor_look,
   nh_bathroom,
+  nh_bathroom_top,
+  nh_hall,
   nh_kid_section,
   nh_kitchen,
+  nh_kitchen_closeup,
   nh_living_room,
+  nh_living_room_wide,
   nh_room,
+  nh_room_four_beds,
+  nh_room_three_beds,
   oh_bathroom,
   oh_kitchen,
   oh_living_room,
   oh_room,
   pricelist_header,
-  hero_us,
+  s_arbor_entrance,
+  s_grass,
+  s_hay_bale,
+  s_playground_stairs,
+  s_sauna,
+  s_signpost,
+  w_arbor,
+  w_arbor_bottom,
+  w_arbor_interior,
+  w_arbor_interior2,
+  w_arbor_top,
+  w_oh_full,
+  w_oh_full2,
+  w_oh_icicle,
+  w_oh_terrace,
+  w_sauna,
 };
