80df5f66c5c938b8f964a43a8bc9fc321424022a
quni <quni.inne@gmail.com>
2024-05-29 18:21:38 +0200

    Logo: add possibility to stylize

--- src/components/UI/Logo/Logo.tsx
+++ src/components/UI/Logo/Logo.tsx
@@ -1,12 +1,13 @@
 import { logo } from "../../../utils/assets";
 
-interface LogoProps {}
+interface LogoProps {
+  className?: string;
+}
 
-const Logo: React.FC<LogoProps> = () => {
+const Logo: React.FC<LogoProps> = ({ className }) => {
   return (
     <div className="flex items-center gap-2">
-      <img src={logo} alt="Logo" className="h-auto w-[100px] " />
-      {/* <span className="text-text">Stanica Podkowa</span> */}
+      <img src={logo} alt="Logo" className={`h-auto w-[100px] ${className}`} />
     </div>
   );
 };
