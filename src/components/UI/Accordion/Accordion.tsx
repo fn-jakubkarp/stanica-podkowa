4cbcfa06a5c12e0246a199963d9004bf08d4dc49
quni <quni.inne@gmail.com>
2024-05-29 18:17:17 +0200

    style: fix text alignment [Accordion]

--- src/components/UI/Accordion/Accordion.tsx
+++ src/components/UI/Accordion/Accordion.tsx
@@ -10,12 +10,12 @@ const Accordion: React.FC<AccordionProps> = ({ question, answer }) => {
   const [tabOpen, setTabOpen] = useState(false);
 
   return (
-    <div className="py-2">
+    <div className="">
       <button
         onClick={() => setTabOpen(!tabOpen)}
         className="flex w-full flex-nowrap items-start justify-between"
       >
-        <span className=" text-sm">{question}</span>
+        <span className="text-left">{question}</span>
         {tabOpen ? (
           <RiArrowDropDownFill className="rotate-180 text-xl" />
         ) : (
@@ -23,9 +23,9 @@ const Accordion: React.FC<AccordionProps> = ({ question, answer }) => {
         )}
       </button>
       <div
-        className={`text-text grid overflow-hidden text-sm opacity-90 transition-all duration-300 ease-in-out ${tabOpen ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"}`}
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out ${tabOpen ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"}`}
       >
-        <div className="overflow-hidden ">{answer}</div>
+        <div className="overflow-hidden px-4">{answer}</div>
       </div>
     </div>
   );
