38fbb1846c3e37336c3562b47ba2c8498ac14581
quni <quni.inne@gmail.com>
2024-06-05 21:39:53 +0200

    Center Contact section

--- src/components/sections/Contact/Contact.tsx
+++ src/components/sections/Contact/Contact.tsx
@@ -7,29 +7,29 @@ const Contact: React.FC = () => {
     <>
       <section
         id="kontakt"
-        className="mx-auto my-8 flex h-auto max-w-screen-2xl flex-col justify-center gap-4 px-4 lg:px-0"
+        className="mx-auto my-8 flex h-auto max-w-screen-2xl flex-col justify-center  px-4 lg:px-0"
       >
         <h3 className="flex justify-center font-bold lg:text-5xl">SKONTAKTUJ SIĘ</h3>
-        <p className="">
-          Jeśli masz pytania lub chcesz poznać szczegóły zadzwoń
+        <p className="justify-center items-center text-center flex mt-2">
+          Masz pytania lub chcesz poznać szczegóły zadzwoń?
         </p>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2 max-w-screen-xl w-full mx-auto justify-center items-center mt-4">
           <a
             onClick={() => handleCall(phoneNumbers.kamil)}
             className="flex items-center gap-2"
           >
-            <MdLocalPhone /> Kamil: {phoneNumbers.kamil}
+            <MdLocalPhone /> {phoneNumbers.kamil}
           </a>
           <a
             onClick={() => handleCall(phoneNumbers.katarzyna)}
             className="flex items-center gap-2"
           >
             <MdLocalPhone />
-            Katarzyna: {phoneNumbers.katarzyna}
+            {phoneNumbers.katarzyna}
           </a>
           <p className="flex items-center gap-2">
             <MdEmail />
-            Email: stanicapodkowa@gmail.com
+            stanicapodkowa@gmail.com
           </p>
         </div>
       </section>
