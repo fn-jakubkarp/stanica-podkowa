f92aa8e4a4482eff2732bb5748b33c846769af4c
quni <quni.inne@gmail.com>
2024-06-05 19:12:11 +0200

    footer: adjust max-w

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <>
       <footer className="">
-        <div className="mx-auto w-full max-w-screen-2xl p-4 py-6 lg:py-8">
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <a href="#" className="flex items-center">
