7e3c6051972b577b96646c971b572381fe84dcb7
quni <quni.inne@gmail.com>
2024-06-05 16:53:42 +0200

    feat: add lightbox to /gallery

---
+++ src/utils/lightboxSlides.ts
@@ -0,0 +1,88 @@
+import * as src from "../utils/assets";
+
+export const mansionSlides = [
+  {
+    src: src.m_arbor,
+  },
+  {
+    src: src.m_new_home,
+  },
+  {
+    src: src.m_outdoor_look,
+  },
+];
+
+export const interiorSlides = [
+  {
+    src: src.nh_bathroom_top,
+  },
+  {
+    src: src.nh_hall,
+  },
+  {
+    src: src.nh_kitchen_closeup,
+  },
+  {
+    src: src.nh_living_room_wide,
+  },
+  {
+    src: src.nh_room_four_beds,
+  },
+  {
+    src: src.nh_room_three_beds,
+  },
+];
+
+export const winterSlides = [
+  {
+    src: src.w_arbor,
+  },
+  {
+    src: src.w_arbor_bottom,
+  },
+  {
+    src: src.w_arbor_interior,
+  },
+  {
+    src: src.w_arbor_interior2,
+  },
+  {
+    src: src.w_arbor_top,
+  },
+  {
+    src: src.w_oh_full,
+  },
+  {
+    src: src.w_oh_full2,
+  },
+  {
+    src: src.w_oh_icicle,
+  },
+  {
+    src: src.w_oh_terrace,
+  },
+  {
+    src: src.w_sauna,
+  },
+];
+
+export const summerSlides = [
+  {
+    src: src.s_arbor_entrance,
+  },
+  {
+    src: src.s_grass,
+  },
+  {
+    src: src.s_hay_bale,
+  },
+  {
+    src: src.s_playground_stairs,
+  },
+  {
+    src: src.s_sauna,
+  },
+  {
+    src: src.s_signpost,
+  },
+];
