cf3454b3007cb60296802352e246fa6805e006f3
quni <quni.inne@gmail.com>
2024-07-06 14:39:57 +0200

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
+// import react from "@vitejs/plugin-react-swc";
+// import { defineConfig } from "vite";
+// import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+// export default defineConfig({
+//   server: {
+//     host: "0.0.0.0",
+//   },
+//   plugins: [
+//     react(),
+//     ViteImageOptimizer({
+//       jpg: {
+//         quality: 85,
+//       },
+//       png: {
+//         quality: 10,
+//       },
+//     }),
+//   ],
+//   build: {
+//     outDir: "dist",
+//     assetsDir: "assets",
+//     rollupOptions: {
+//       output: {
+//         assetFileNames: (assetInfo) => {
+//           let extType = assetInfo.name.split(".").at(1);
+//           if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+//             extType = "img";
+//           }
+//           return `assets/${extType}/[name]-[hash][extname]`;
+//         },
+//         chunkFileNames: "assets/js/[name]-[hash].js",
+//         entryFileNames: "assets/js/[name]-[hash].js",
+//       },
+//     },
+//   },
+// });
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
@@ -21,5 +57,20 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name) {
+            const extType = assetInfo.name.split(".").at(1);
+            if (extType && /png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              return `assets/img/[name]-[hash][extname]`;
+            }
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
   },
 });
