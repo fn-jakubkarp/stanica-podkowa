efdea18515f8f1536a256c2134d19d3eee7435b0
jakubkarp <quni.inne@gmail.com>
2024-10-20 15:54:18 +0200

    remove unused fb button from footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -96,29 +96,6 @@ const Footer: React.FC<FooterProps> = () => {
               © 2024 <a className="hover:underline">Stanica Podkowa™</a>.
               Wszelkie prawa zastrzeżone.
             </span>
-            <div className="mt-4 flex sm:mt-0 sm:justify-center">
-              <a
-                href="https://www.facebook.com/agroturystykapodkowa/"
-                className=" "
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <svg
-                  className="h-4 w-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 8 19"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span className="sr-only">Facebook page</span>
-              </a>
-            </div>
           </div>
         </div>
       </footer>
