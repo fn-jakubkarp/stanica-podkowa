729414de329bc6015101e4764b3b634f8efccf02
jakubkarp <quni.inne@gmail.com>
2024-10-20 16:59:30 +0200

    add variant to navbar

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -4,38 +4,39 @@ import { NavLink } from "react-router-dom";
 
 interface NavLinksProps {
   closeDrawer: () => void;
+  variant?: "light" | "dark";
 }
 
-const NavLinks: React.FC<NavLinksProps> = ({ closeDrawer }) => {
+const NavLinks: React.FC<NavLinksProps> = ({
+  closeDrawer,
+  variant = "light",
+}) => {
+  const linkClasses =
+    variant === "dark"
+      ? "text-white hover:underline lg:h-full lg:text-2xl"
+      : "text-black hover:underline lg:h-full lg:text-2xl";
+
   return (
     <>
-      <NavLink
-        to="/"
-        className="hover:underline lg:h-full lg:text-2xl"
-        onClick={closeDrawer}
-      >
+      <NavLink to="/" className={linkClasses} onClick={closeDrawer}>
         Strona główna
       </NavLink>
-      <NavLink
-        to="/cennik"
-        className="hover:underline lg:h-full lg:text-2xl"
-        onClick={closeDrawer}
-      >
+      <NavLink to="/cennik" className={linkClasses} onClick={closeDrawer}>
         Cennik
       </NavLink>
-      <NavLink
-        to="/galeria"
-        className="hover:underline lg:h-full lg:text-2xl"
-        onClick={closeDrawer}
-      >
+      <NavLink to="/galeria" className={linkClasses} onClick={closeDrawer}>
         Galeria
       </NavLink>
     </>
   );
 };
 
-const Navbar: React.FC = () => {
-  //   Drawer logic
+interface NavbarProps {
+  variant?: "light" | "dark";
+}
+
+const Navbar: React.FC<NavbarProps> = ({ variant = "light" }) => {
+  // Drawer logic
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
@@ -45,14 +46,16 @@ const Navbar: React.FC = () => {
     setIsDrawerOpen(false);
   };
 
+  const buttonColor = variant === "dark" ? "text-white" : "text-black";
+
   return (
     <>
-      <nav className="flex justify-end sm:w-1/2 lg:h-full lg:items-center">
+      <nav className={`flex justify-end sm:w-1/2 lg:h-full lg:items-center`}>
         <div className="hidden h-full w-full items-center justify-between md:flex">
-          <NavLinks closeDrawer={closeDrawer} />
+          <NavLinks closeDrawer={closeDrawer} variant={variant} />
         </div>
         <div className="h-[24px] md:hidden">
-          <button onClick={toggleDrawer} className="">
+          <button onClick={toggleDrawer} className={buttonColor}>
             {isDrawerOpen ? (
               <IoCloseOutline size={24} />
             ) : (
@@ -63,7 +66,7 @@ const Navbar: React.FC = () => {
       </nav>
       {isDrawerOpen && (
         <div className="flex basis-full flex-col items-center gap-2 pt-8 text-2xl">
-          <NavLinks closeDrawer={closeDrawer} />
+          <NavLinks closeDrawer={closeDrawer} variant={variant} />
         </div>
       )}
     </>
