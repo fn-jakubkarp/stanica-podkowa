980cd7340ee327658472ac142bb7357bd92fe444
jakubkarp <quni.inne@gmail.com>
2024-10-20 16:43:10 +0200

    add variants to logo component

--- src/components/UI/Logo/Logo.tsx
+++ src/components/UI/Logo/Logo.tsx
@@ -1,13 +1,20 @@
-import { logo } from "../../../utils/assets";
+import { logo, logoDark } from "../../../utils/assets";
 
 interface LogoProps {
   className?: string;
+  variant?: "light" | "dark";
 }
 
-const Logo: React.FC<LogoProps> = ({ className }) => {
+const Logo: React.FC<LogoProps> = ({ className, variant = "light" }) => {
+  const logoSrc = variant === "dark" ? logoDark : logo;
+
   return (
     <div className="flex items-center gap-2">
-      <img src={logo} alt="Logo" className={`h-auto w-[100px] lg:w-[175px] ${className}`} />
+      <img
+        src={logoSrc}
+        alt="Logo"
+        className={`h-auto w-[100px] lg:w-[175px] ${className}`}
+      />
     </div>
   );
 };
