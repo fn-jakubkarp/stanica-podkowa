a44280b6077df0631f346cf0d977c70746ae065f
jakubkarp <quni.inne@gmail.com>
2024-10-20 17:02:10 +0200

    add navigation to logo

--- src/components/UI/Logo/Logo.tsx
+++ src/components/UI/Logo/Logo.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { logo, logoDark } from "../../../utils/assets";
 
 interface LogoProps {
@@ -6,14 +7,20 @@ interface LogoProps {
 }
 
 const Logo: React.FC<LogoProps> = ({ className, variant = "light" }) => {
+  const navigate = useNavigate();
   const logoSrc = variant === "dark" ? logoDark : logo;
+
+  const handleClick = () => {
+    navigate("/");
+  };
 
   return (
     <div className="flex items-center gap-2">
       <img
         src={logoSrc}
         alt="Logo"
-        className={`h-auto w-[100px] lg:w-[175px] ${className}`}
+        className={`h-auto w-[100px] lg:w-[175px] ${className} cursor-pointer`}
+        onClick={handleClick}
       />
     </div>
   );
